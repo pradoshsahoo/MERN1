7cacd69f9efc98a7e51f3234f6adc9451d01a765
Pradosh Sahoo <pradoshsahoo81@gmail.com>
2023-06-01 21:36:27 +0530

    Minor improvement to assignment

--- Day_4_Assignment/add.js
+++ Day_4_Assignment/add.js
@@ -4,28 +4,32 @@ button.addEventListener("click", () => {
   const image = document.getElementById("image").value;
   const rating = document.getElementById("rating").value;
   const desc = document.getElementById("desc").value;
-  fetch("http://localhost:5501/data", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      title: title,
-      rating: rating,
-      image: image,
-      desc: desc,
-      id: Date.now(),
-    }),
-  })
-    .then((response) => {
-      if (response.ok) {
-        alert("Food added successfully");
-        return response.json();
-      } else {
-        alert("Something went wrong");
-      }
+  if (title != "" || image != "" || rating != "" || desc != "") {
+    fetch("http://localhost:5501/data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title,
+        rating: rating,
+        image: image,
+        desc: desc,
+        id: Date.now(),
+      }),
     })
-    .then((data) => {
-      window.location.href = "./index.html";
-    });
+      .then((response) => {
+        if (response.ok) {
+          alert("Food added successfully");
+          return response.json();
+        } else {
+          alert("Something went wrong");
+        }
+      })
+      .then((data) => {
+        window.location.href = "./index.html";
+      });
+  } else {
+    document.getElementById("error").innerText = "Fill all the fields";
+  }
 });
